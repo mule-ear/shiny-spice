259f0ad25617d7b55a999c5888b7eef2ba58b0c3
mule-ear <mark.winnemueller@codefutures.com>
2015-04-12 23:57:43 -0600

    Added some more comments to the program

--- SevSegmentDriver9.ino
+++ SevSegmentDriver9.ino
@@ -1,5 +1,4 @@
-
-#include <Wire.h>
+// Written by mule-ear on or about 2015/04/14
 
 int clockPulse[2] = {10 , 11};
 //int digit2Enable = 11;
@@ -13,7 +12,17 @@ I numbered the segments in the order of the pins on my display
 pin 1(Arduiono 2) is the left bottom, 2(3) is the bottom, 3 is ground (skipped), 4(4) is right bottom and 5(5) is the decimal (not used, but wired up anyway)
 pin 6(6) is the right top, 7(7) is the top, 8(skipped) is ground, 9(8) is the left top and 10(9) is the center element.
 That's how I got the values for chr[] - If I want the left bottom segment I use the 1st element of the array - e.g. 0b00000001 
-Pin 2 is the LSB of my chr[] value
+Pin 2 is the LSB of my chr[] value. Or, another way too say it is:
+Arduino pin 2 goea to pin 1 on the display
+3 -> 2
+4 -> 4
+5 -> 5
+6 -> 6
+7 -> 7
+8 -> 9
+9 -> 10 
+All through the D Type Flip-Flop, of course.
+If I can figure out how to design a new part in Frizing, I'll include a Fritzing project.
 */
 
 void setup()
