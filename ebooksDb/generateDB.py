6045dade525148bd8c7036d6f62dd0db6dc22810
mule-ear <mark.winnemueller@codefutures.com>
2017-04-05 12:06:46 -0600

    Changed logging to use the conf/logging.conf file

--- ebooksDb/generateDB.py
+++ ebooksDb/generateDB.py
@@ -1,100 +1,103 @@
 #! /usr/bin/env python3
 
-import os, sys, sqlite3, logging
+import os, sys, sqlite3, logging, logging.config
 
-logging.basicConfig(filename='/tmp/generator.log',level=logging.DEBUG)
-
-sqCon = sqlite3.Connection(database="proj.db")
-# Going to start with 2 cursors
-sqCur1 = sqCon.cursor()
-sqCur2 = sqCon.cursor()
-
-# get basedir from command-line, conf file, or store preferences in sqlite3
-# but for now:
-basedir = '/mnt/Storage1/EBooks'
-
-def generate_directories_table():
+def generate_directories_table(baseDir, cur, log):
     # Populate dirs table 
     # CREATE TABLE directories (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR, path VARCHAR, full_path VARCHAR);
     # The idea being that the dir will have its own name, path is the parent, full_path is what I'll add 
+    log.info("generating data for directories table")
+    cur.execute("DROP TABLE IF EXISTS directories;")
+    cur.execute("CREATE TABLE directories (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR, path VARCHAR, full_path VARCHAR);")
 
-    sqCur1.execute("DROP TABLE IF EXISTS directories;")
-    sqCur1.execute("CREATE TABLE directories (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR, path VARCHAR, full_path VARCHAR);")
-
-    listOfDirs = [x[0] for x in os.walk(basedir)]
+    listOfDirs = [x[0] for x in os.walk(baseDir)]
 
     for i in listOfDirs:
         # insertValues = (i.rsplit('/',1)[1] , i.rsplit('/',1)[0] , i)
         # sqCur1.execute("INSERT INTO directories (name, path, full_path) VALUES(?,?,?)", insertValues)
-        sqCur1.execute("INSERT INTO directories (name, path, full_path) VALUES(?,?,?)", (i.rsplit('/',1)[1]+'/' , i.rsplit('/',1)[0]+'/' , i+'/'))
+        cur.execute("INSERT INTO directories (name, path, full_path) VALUES(?,?,?)", (i.rsplit('/',1)[1]+'/' , i.rsplit('/',1)[0]+'/' , i+'/'))
             
-def create_tables_for_books():
+def create_tables_for_books(cur, log):
+    log.info("Creating Tables")
     # CREATE the tables needed for books
     #
 
-    sqCur1.execute("DROP TABLE IF EXISTS books;")
-    sqCur1.execute("DROP TABLE IF EXISTS files;")
-    sqCur1.execute("DROP TABLE IF EXISTS types;")
-    sqCur1.execute("DROP TABLE IF EXISTS authors;")
-    sqCur1.execute("DROP TABLE IF EXISTS tags;")
-    sqCur1.execute("DROP TABLE IF EXISTS categories;")
-    sqCur1.execute("DROP TABLE IF EXISTS books_tags;")
-    sqCur1.execute("DROP TABLE IF EXISTS books_categories;")
+    cur.execute("DROP TABLE IF EXISTS books;")
+    cur.execute("DROP TABLE IF EXISTS files;")
+    cur.execute("DROP TABLE IF EXISTS types;")
+    cur.execute("DROP TABLE IF EXISTS authors;")
+    cur.execute("DROP TABLE IF EXISTS tags;")
+    cur.execute("DROP TABLE IF EXISTS categories;")
+    cur.execute("DROP TABLE IF EXISTS books_tags;")
+    cur.execute("DROP TABLE IF EXISTS books_categories;")
 
-    sqCur1.execute("CREATE TABLE types (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR, extension VARCHAR);")
-    sqCur1.execute("CREATE TABLE files (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR, path INTEGER, type INTEGER);")
-    sqCur1.execute("CREATE TABLE authors (id INTEGER PRIMARY KEY AUTOINCREMENT, first_name VARCHAR, last_name VARCHAR);")
-    sqCur1.execute("CREATE TABLE tags (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR);")
-    sqCur1.execute("CREATE TABLE categories (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR );")
-    sqCur1.execute("CREATE TABLE books_tags (book_id INTEGER, tags_id INTEGER, PRIMARY KEY (book_id, tags_id));")
-    sqCur1.execute("CREATE TABLE books_categories (book_id INTEGER, category_id INTEGER, PRIMARY KEY(book_id, category_id ));")
-    sqCur1.execute("CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR, path INTEGER, type INTEGER, author INTEGER DEFAULT NULL, \
+    cur.execute("CREATE TABLE types (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR, extension VARCHAR);")
+    cur.execute("CREATE TABLE files (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR, path INTEGER, type INTEGER);")
+    cur.execute("CREATE TABLE authors (id INTEGER PRIMARY KEY AUTOINCREMENT, first_name VARCHAR, last_name VARCHAR);")
+    cur.execute("CREATE TABLE tags (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR);")
+    cur.execute("CREATE TABLE categories (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR );")
+    cur.execute("CREATE TABLE books_tags (book_id INTEGER, tags_id INTEGER, PRIMARY KEY (book_id, tags_id));")
+    cur.execute("CREATE TABLE books_categories (book_id INTEGER, category_id INTEGER, PRIMARY KEY(book_id, category_id ));")
+    cur.execute("CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR, path INTEGER, type INTEGER, author INTEGER DEFAULT NULL, \
                     CONSTRAINT directories_id FOREIGN KEY (path) REFERENCES directories(id),\
                     CONSTRAINT type_id FOREIGN KEY (type) REFERENCES types(id), \
                     CONSTRAINT author_id FOREIGN KEY (author) REFERENCES authors(id));")
 
-def does_type_exist(a_type):
+def does_type_exist(a_type, cur, log):
     a_type = a_type.lower()
-    sqCur1.execute("SELECT id FROM types WHERE extension = ?",(a_type,))
-    result = sqCur1.fetchone()
+    cur.execute("SELECT id FROM types WHERE extension = ?",(a_type,))
+    result = cur.fetchone()
     if result is None:
-        sqCur1.execute("INSERT INTO types(name,extension) VALUES(NULL,?)", (a_type,))
-        logging.info("Adding type {} to types table".format(a_type))
+        cur.execute("INSERT INTO types(name,extension) VALUES(NULL,?)", (a_type,))
+        log.info("Adding type {} to types table".format(a_type))
     else:
-        logging.debug("type exists = {}".format(result))
+        log.debug("type exists = {}".format(result))
 
-def populate_files_table():
+def populate_files_table(cur, log):
+    log.info("Populating files trable")
 	# even though I could have gotten this from generate_directories_table() I thought it would be confusing
     listOfDirs = [x[0] for x in os.walk(basedir)]
 
     for dir1 in listOfDirs:
 
         for entry in os.listdir(dir1):
-            logging.debug("entry = " + entry)
+            log.debug("entry = " + entry)
             # make sure it's a file (and not a dir) and it has an extension
             if (os.path.isfile(os.path.join(dir1, entry)) and '.' in entry):
-                logging.debug("Is a file " + dir1+ '/' + entry)
+                log.debug("Is a file " + dir1+ '/' + entry)
                 # make sure it has an extension
-                logging.debug("dir ="+ dir1)
-                logging.debug("full_name = " + entry)
-                sqCur1.execute("SELECT id FROM directories WHERE full_path = ?",(dir1 + '/',))
+                log.debug("dir ="+ dir1)
+                log.debug("full_name = " + entry)
+                cur.execute("SELECT id FROM directories WHERE full_path = ?",(dir1 + '/',))
                 result = sqCur1.fetchone()
-                logging.debug("result = " + str(result))
+                log.debug("result = " + str(result))
                 name, ext = entry.rsplit('.',1)
-                does_type_exist(ext)
+                does_type_exist(ext, cur, log)
                 query = "INSERT INTO files(name, path, type) VALUES(?,(SELECT id FROM directories WHERE full_path = ?) , (SELECT id FROM types WHERE extension = ?))"            
-                logging.debug("name = {0}, ext = {1}".format(name, ext))
+                log.debug("name = {0}, ext = {1}".format(name, ext))
                 query_values = (name, dir1 + '/', ext)
                 sqCur1.execute(query, query_values)
                 
 if __name__ == "__main__":
+	
+    logging.config.fileConfig('conf/logging.conf')
+    logger = logging.getLogger('generateDB')
+	
+    #logging.basicConfig(filename='/tmp/generator.log',level=logging.DEBUG)
 
-    generate_directories_table()
-    create_tables_for_books()
-    populate_files_table()
+    sqCon = sqlite3.Connection(database="proj.db")
+	# Going to start with 2 cursors
+    sqCur1 = sqCon.cursor()
+    sqCur2 = sqCon.cursor()
+	
+	# get basedir from command-line, conf file, or store preferences in sqlite3
+	# but for now:
+    basedir = '/mnt/Storage1/EBooks'
 
+    generate_directories_table(basedir,sqCur1,logger)
+    create_tables_for_books(sqCur1, logger)
+    populate_files_table(sqCur1, logger)
 
-            
-sqCon.commit()
-sqCon.close()
+    sqCon.commit()
+    sqCon.close()
 
